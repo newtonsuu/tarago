3d03d743cba2a1301794358548aee9186a850485
newtonsuu <kingjerichojames@gmail.com>
2025-05-08 17:58:00 +0800

    feat: restructure as deployable PWA (index.html, manifest, service worker, iOS icon)

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "tarago-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return r;
+    }).catch(() => caches.match(e.request).then(m => m || caches.match("./index.html")))
+  );
+});
